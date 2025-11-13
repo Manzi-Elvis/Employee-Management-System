cc09be3aeef098140bab9f7fbf225774580414fc
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-13 07:03:05 +0200

    GET and ADD employee methods in routes

--- Backend/Routes/employeeRoutes.js
+++ Backend/Routes/employeeRoutes.js
@@ -1,3 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const emp = require('../Models/Employee');
+
+// Get all employees(with department details)
+router.get('/' , async(req, res) => {
+      try{
+            const employees = await Employee.find().populate('department_id', 'department_name');
+            res.json({success: true, data: employees })
+      }
+      catch(err){
+            res.status(500).json({success: false, message: err.message});
+      }
+      
+});
+
+//Add an employee
+router.post('/' , async(req, res) => {
+      try{
+            const employee = new Employee(req.body)
+            await employee.save();
+            res.json({success: true, message: "Employee added Successfully", data: employee});
+      }
+      catch(err){
+            res.status(400).json({success: false, message: err.message });
+      }
+});
