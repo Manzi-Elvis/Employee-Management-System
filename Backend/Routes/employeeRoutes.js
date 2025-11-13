6c5d398ae58a90d9b4528be71444b0f1d4a2d9f6
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-13 11:19:56 +0200

    Update, delete and get employees by department

--- Backend/Routes/employeeRoutes.js
+++ Backend/Routes/employeeRoutes.js
@@ -25,3 +25,39 @@ router.post('/' , async(req, res) => {
             res.status(400).json({success: false, message: err.message });
       }
 });
+//Update an employee(e.g salary, position, department)
+router.put('/:id' , async(req, res) => {
+      try{
+            const employee = await Employee.findByIdAndUpdate(req.params.id, req.body, {new: true});
+            if(!employee){
+                  return res.status(404).json({success: false, message: 'Employee not found'});
+            }
+            res.json({success: true, message: 'Employee updated successfully', data: employee});
+      }
+      catch(err){
+            res.status(400).json({success: false, message: err.message});
+      }
+});
+//Delete an employee
+router.delete('/:id' , async(req, res) => {
+      try{
+            await Employee.findByIdAndDelete(req.params.id);
+            res.json({success: true, message: 'Employee deleted successfully'});
+      }
+      catch(err){
+            res.status(400).json({success: false, message: err.message});
+      }
+});
+
+// Get employees by department
+router.get('/department/:deptId' , async(req, res) => {
+      try{
+            const employees = await Employee.find({department_id: req.params.deptId}).populate('department_id', 'department_name');
+            res.json({success: true, data: employees });
+      }
+      catch(err){
+            res.status(400).json({success: false, message: err.message});
+      }
+});
+
+module.exports = router;
