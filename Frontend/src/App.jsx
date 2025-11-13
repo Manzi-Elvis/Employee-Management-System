05f1e95099cf789145d09f27fc3f19664bd346ca
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-13 21:13:57 +0200

    Linking

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
-import Employees from './pages/Employees';
+import Login from './pages/Login';
 import Departments from './pages/Departments';
-
-function App() {
+import Employees from './pages/Employees';
+import Navbar from './components/Navbar';
+import ProtectedRoute from './components/ProtectedRoute';
+export default function App(){
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/employees" element={<Employees />} />
-        <Route path="/departments" element={<Departments />} />
-      </Routes>
-    </Router>
-  );
+    <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login/>} />
+          <Route path="/" element={<ProtectedRoute><Dashboard/></ProtectedRoute>} />
+          <Route path="/departments" element={<ProtectedRoute><Departments/></ProtectedRoute>} />
+          <Route path="/employees" element={<ProtectedRoute><Employees/></ProtectedRoute>} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+    </BrowserRouter>
+);
 }
-
-export default App;
