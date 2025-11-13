c142da5c8c4b925b835a07363856fc0de563f379
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-13 11:40:11 +0200

    debugged

--- Backend/Routes/departmentRoutes.js
+++ Backend/Routes/departmentRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 const Department = require('../Models/Department');
 
 // Get all departments
