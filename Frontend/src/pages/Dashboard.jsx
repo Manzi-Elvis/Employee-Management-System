6979ac221c11d4065f1be49e8894499a27e1e143
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-11-13 21:11:39 +0200

    Dashboard page

---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+export default function Dashboard() {
+    return (
+      <div className='container'>
+        <h2>Dashboard</h2>
+        <div className='card'>
+          <p>Welcome to the Employee Management System</p>
+        </div>
+      </div>
+    );
+};
